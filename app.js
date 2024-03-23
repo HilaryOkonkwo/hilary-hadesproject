0fd12be75cdf3bdb88d63665aeb61e61cbb49a77
HilaryOkonkwo <98573016+HilaryOkonkwo@users.noreply.github.com>
2024-03-24 00:59:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3003;
 
 // Set up MySQL connection
 const db = mysql.createConnection({
-  host: '159.138.164.61',
+  host: '192.168.2.24',
   user: 'sammy',
   password: '#Password123',
   database: 'hilary',
